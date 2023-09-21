1e6996f9c812e0bf1fb2812c500dd2b7b53c9549
r.aramide <r.aramide@lagetronix.com>
2023-09-21 16:27:58 +0100

    added github folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
 import Die from "./Die";
